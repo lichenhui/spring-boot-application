93c7dc1f0da6e55412dc4e115c35c6845ebdf3d5
lichenhui <lichenhuiustb@gmail.com>
2016-07-19 11:46:30 +0800

    init

---
+++ src/main/java/cn/lichenhui/App.java
@@ -0,0 +1,13 @@
+package cn.lichenhui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
